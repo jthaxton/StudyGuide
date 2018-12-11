b8691ef7e1c627139872db7f3133a6aeed971ab5
Joe Thaxton <14jthaxton@gmail.com>
2018-12-11 13:54:52 -0800

    algo practice

--- algorithms.rb
+++ algorithms.rb
@@ -15,6 +15,31 @@ def paren_balanced?(str)
     end 
     true 
 end 
+# other balanced parens
+def is_valid(s)
+    return false if s.length % 2 != 0
+    # return true if s.length == 0
+    hash = {'('=>')','{'=>'}','['=>']'}
+    spl = s.split('')
+    full = false
+    spl.each_with_object([]) do |el, stack|
+        if hash[el]
+            stack << el
+        elsif hash.values.include?(el)
+            return false unless hash[stack.pop] == el
+        else 
+            return false
+        end 
+        full = true if stack.length > 0
+        full = false if stack.length == 0
+    end 
+    if full == true 
+        return false 
+    else 
+        return true 
+    end 
+    
+end
 
 # operations required for strings to match 
 def similar_strings(a,b)
@@ -61,3 +86,42 @@ def three_sum(nums)
   end 
   return false 
 end
+
+# returns all triplets 
+# def three_sum_all(num)
+#     found = false
+#     nums.sort!
+#     result = []
+#   i = 0 
+#   while i < nums.length - 1
+
+#     j = i + 1 
+#     k = nums.length - 1 
+#     while i < k 
+#       if nums[i] + nums[j] + nums[k] == 0
+#         result << [nums[i], nums[j], nums[k]]
+#         j += 1 
+#         k -= 1 
+#         found = true 
+#       elsif nums[i] + nums[j] + nums[k] < 0 
+#         j += 1 
+#       elsif nums[i] + nums[j] + nums[k] > 0 
+#         k -= 1
+#       end 
+#     end 
+    
+#   end 
+#   if found == false 
+#     return [] 
+#   end 
+#   return result 
+# end 
+
+# this time complexity is pathetic. fix it.
+def num_unique_emails(emails)
+    result = []
+    emails.each do |el|         
+        result << [el.split('').delete_if{|e| e == '.'}[0],'@',el.split('@')[-1]].join('')
+    end 
+    result.uniq.length
+end 
