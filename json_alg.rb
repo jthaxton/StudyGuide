2d621bb480893c7572b9d672ef3f874577d8cca4
Joe Thaxton <14jthaxton@gmail.com>
2018-12-20 12:43:54 -0800

    last commit

--- json_alg.rb
+++ json_alg.rb
@@ -1,6 +1,5 @@
 require 'json'
 
-
 class Store
   attr_accessor :store, :hashes
   def initialize 
@@ -9,79 +8,80 @@ class Store
   end
 
 # my implementation of ruby's json recurse_proc
-  def recurse_proc(el, result)
-    case result
-    when Array
-      result.each { |x| recurse_proc el, x}
-    when Hash
-      result.each do |x, y|
-        
-        key_exists = result.key?(x) && result[x] == y
-        if key_exists && !@hashes[x]
-          if result[x].class == Array
-            @hashes[x] = {}
-            result[x].uniq.each do |item|
-              if @hashes[x][item].nil?
-                @hashes[x][item] = [el]
-              else
-                @hashes[x][item] << el
-              end
-            end
-          else
-
-            @hashes[x] = {result[x] => [el]}
-          end
-        elsif key_exists && @hashes[x] && @hashes[x][result[x]].class == Array
-          @hashes[x][result[x]] << (el)
-        elsif key_exists && @hashes[x] && !@hashes[x][result[x]]
-          if result[x].class == Array
-            result[x].uniq.each do |item|
-              if @hashes[x][item].nil?
-                @hashes[x][item] = [el]
-              else
-                @hashes[x][item] << el
-              end
-            end
-          else
-
-            @hashes[x][result[x]] = [el]
-          end
-        end 
-        recurse_proc el, x; recurse_proc el, y 
-      end 
-    else
-      result
-    end
-
-end
-
-  def recurse(el, input, res)
-    case input
-    when Array
-      input.each do |item|
-        if !@hashes['list'][item].nil?
-          @hashes['list'][item].each do |id|
-            if @store[id] && input.all?{|element| @store[id]['list'].count(element) >= input.count(element) }
-              res << id
-            end
-          end
+    def recurse_add(el, result)
+        case result
+        when Array
+            result.each { |x| recurse_add el, x}
+        when Hash
+            result.each do |x, y|
+                key_exists = result.key?(x) && result[x] == y
+                if key_exists && !@hashes[x]
+                    if result[x].class == Array
+                        @hashes[x] = {}
+                        result[x].uniq.each do |item|
+                            if @hashes[x][item].nil?
+                                @hashes[x][item] = [el]
+                            else
+                                @hashes[x][item] << el
+                            end
+                        end
+                    else
+                    @hashes[x] = {result[x] => [el]}
+                    end
+                elsif key_exists && @hashes[x] && @hashes[x][result[x]].class == Array
+                    @hashes[x][result[x]] << (el)
+                elsif key_exists && @hashes[x] && !@hashes[x][result[x]]
+                    if result[x].class == Array
+                        result[x].uniq.each do |item|
+                            if @hashes[x][item].nil?
+                                @hashes[x][item] = [el]
+                            else
+                                @hashes[x][item] << el
+                            end
+                        end
+                    else
+                        @hashes[x][result[x]] = [el]
+                    end
+                end 
+                recurse_add el, x; recurse_add el, y 
+            end 
+        else
+        result
         end
-      end
-    when Hash
-      input.each do |x, y|
-        key_exists = input.key?(x) && input[x] == y
-        if key_exists && @hashes[x] && @hashes[x][input[x]]
-          if @hashes[x][input[x]].class != Hash && x != "type" 
-            res << @hashes[x][input[x]]
-          end
-        end 
-        recurse el, x, res; recurse el, y, res
-      end 
-    else
-      res
     end
-    res
-end
+
+    def recurse_get(el, input, res)
+        case input
+        when Array
+        input.each do |item|
+          @hashes.each do |k,v|
+            if !@hashes[k][item].nil?
+                @hashes[k][item].each do |id|
+
+                    if @store[id] && input.all?{|element| @store[id][k].class == Array ? @store[id][k].count(element) >= input.count(element) : nil}
+                        res << id
+                    end
+                    end
+                end
+            end
+        end
+
+        input.each { |x| recurse_get el, x, res}
+        when Hash
+            input.each do |x, y|
+                key_exists = input.key?(x) && input[x] == y
+                if key_exists && @hashes[x] && @hashes[x][input[x]]
+                    if @hashes[x][input[x]].class != Hash && x != "type" 
+                        res << @hashes[x][input[x]]
+                    end
+                end 
+                recurse_get el, x, res; recurse_get el, y, res
+            end 
+        else
+            res
+        end
+        res
+    end
 
   def query(input)
     
@@ -100,26 +100,24 @@ end
   def get(input)
     result = {}
     res = []
-    ke = JSON.parse(input[4..input.length - 1])
-
+    parsed_input = JSON.parse(input[4..input.length - 1])
     if @store == {}
       return [] 
     end
-
-    if ke == {}
+    if parsed_input == {}
       return @store.keys.map {|k| @store[k]}
     end 
-    count = ke.keys.length
+    count = parsed_input.keys.length
     r = []
-    ke.each do |k, v|
-      rec = recurse(0, {k => v}, [])
+    parsed_input.each do |k, v|
+      rec = recurse_get(0, {k => v}, [])
       r += rec
     end
     if r[0].class == Array
       return [] if r.length != count
       var = r.inject(:&)
     else
-      var = r.uniq
+      var = r.uniq.sort
     end
     if var.nil?
         return []
@@ -132,34 +130,30 @@ end
   def delete(input)
     res = []
     result = {}
-    ke = JSON.parse(input[7..input.length - 1])
-    
+    parsed_input = JSON.parse(input[7..input.length - 1])
     if @store == {}
       return [] 
     end
 
-    count = 0
-      k = ke.keys[count]
-    r = []
-    ke.each do |k, v|
-      r += recurse(0, {k => v}, [])
+    res_arr = []
+    parsed_input.each do |k, v|
+        res_arr += recurse_get(0, {k => v}, [])
     end
-    if r[0].class == Array
-      var = r.inject(:&)
+    if res_arr[0].class == Array
+        intersection_arr = res_arr.inject(:&)
     else
-      var = r
+        intersection_arr = res_arr
     end
-    if var.nil? || var.empty?
+    if intersection_arr.nil? || intersection_arr.empty?
         return []
     end
-
-    var.each {|el| @store.delete(el)}
+    intersection_arr.each {|el| @store.delete(el)}
   end
 
   def add(input)
-    x = input[4..input.length - 1]
-    @store.merge!({@store.keys.length + 1 => JSON.parse(x)})
-      recurse_proc(@store.keys.length, JSON.parse(x))
+    parsed_input = JSON.parse(input[4..input.length - 1])
+    @store.merge!({@store.keys.length + 1 => parsed_input})
+    recurse_add(@store.keys.length, parsed_input)
   end 
 end
 
